2143c8626480a916908d35defcd9efdd5ad3b331
Alexander Melnichenko <togpe22@gmail.com>
2023-07-01 21:35:55 +0300

    Fixed bag with is_desktop_active()

--- active.py
+++ active.py
@@ -2,7 +2,7 @@ import psutil
 import win32gui
 import win32process
 
-def is_desktop_active():
+def is_desktop_active(APP_NAME):
     hwnd = win32gui.GetForegroundWindow()
     if hwnd == 0:
         # no window is currently active, so assume the desktop is active
@@ -10,8 +10,8 @@ def is_desktop_active():
     else:
         process_id = win32process.GetWindowThreadProcessId(hwnd)[1]
         process_name = psutil.Process(process_id).name()
-        
-        if process_name == 'explorer.exe' or process_name == 'python.exe':
+
+        if process_name == 'explorer.exe' or process_name.split('.')[0] == APP_NAME or process_name == 'python.exe':
             # the active window belongs to the explorer process, so assume the desktop is active
             return True
     return False
